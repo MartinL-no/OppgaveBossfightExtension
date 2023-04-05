2a568838ac4a3e1058ee21956cf51c78102138c2
MartinL-no <martin.lorimer@icloud.com>
2023-04-05 13:22:46 +0200

    rename field

--- OppgaveBossfightExtension/Program.cs
+++ OppgaveBossfightExtension/Program.cs
@@ -5,7 +5,7 @@ namespace OppgaveBossfightExtension
     class Program
     {
         static Random Rand = new Random();
-        private static List<Item> Items;
+        private static List<Item> _items;
 
         static void Main(string[] args)
         {
@@ -15,7 +15,7 @@ namespace OppgaveBossfightExtension
         {
             var randomStrength = Rand.Next(31);
             var isHeroesTurn = Rand.Next(2) == 1;
-            Items = GetRandomListOfItems();
+            _items = GetRandomListOfItems();
 
             var Hero = new GameCharacter("Hero", 100, 20, 40);
             var Boss = new GameCharacter("Boss", 400, randomStrength, 10);
@@ -84,19 +84,19 @@ namespace OppgaveBossfightExtension
             {
                 var healthPotionOrLastItem = GetHealthPotion();
                 hero.PickupItem(healthPotionOrLastItem);
-                Items.Remove(healthPotionOrLastItem);
+                _items.Remove(healthPotionOrLastItem);
             }
             else
             {
-                hero.PickupItem(Items.Last());
-                Items.Remove(Items.Last());
+                hero.PickupItem(_items.Last());
+                _items.Remove(_items.Last());
             }
         }
         static Item GetHealthPotion()
         {
-            var healthPotion = Items.Find(x => x.ItemType == "HealthPotion");
+            var healthPotion = _items.Find(x => x.ItemType == "HealthPotion");
 
-            if (healthPotion == null) return Items.Last();
+            if (healthPotion == null) return _items.Last();
             else return healthPotion;
         }
     }
